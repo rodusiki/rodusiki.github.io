dff8ceae0f4cb47d41e5985132d93e2e67475b61
sblee <seulbee1031@gmail.com>
2024-08-21 17:23:11 +0900

    0821-1

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Kubernetes",
+        "excerpt":"여기는 쿠버네티스 페이지!   ","categories": [],
+        "tags": [],
+        "url": "/00.Kubernetes/",
+        "teaser": null
+      }]
---
+++ index.md
@@ -0,0 +1,9 @@
+---
+layout: home
+author_profile: true
+---
+# Head1
+## Head2
+### Head3
+
+왜 밑줄이 나오는 걸까?
